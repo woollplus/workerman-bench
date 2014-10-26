01c50860ce823feee76b9cca80e090cf712512a7
walkor <worker-man@qq.com>
2014-10-26 14:18:17 +0800

    workerman内核升级，支持backlog

--- applications/Benchmark/Benchmark.php
+++ applications/Benchmark/Benchmark.php
@@ -5,7 +5,7 @@
  * 方法2：使用workerman自带的benchmark软件，只支持64位linux系统
  * ①：./benchmark -n10000 -h1 -c400 -p56789 127.0.0.1    // 命令含义是400并发线程，连接127.0.0.1:56789端口发送一个hello\n扥带服务端返回一个hello\n后断开连接，这样运行10000次
  * ②：./benchmark -n1 -h10000 -c1000 -p56789 127.0.0.1    // 命令含义是1000并发线程，连接127.0.0.1:56789端口连续发送10000个hello\n
- * @author walkor <workerman.net>
+ * @author walkor <walkor@workerman.net>
  */
 class Benchmark extends Man\Core\SocketWorker
 {
